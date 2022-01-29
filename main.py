5adeebd55b3d63180747b0a3d00a132cd8fe1917
enkaranfiles <Sherlock123>
2022-01-29 17:56:39 +0300

    update documentation

--- main.py
+++ main.py
@@ -5,21 +5,35 @@ import pdfplumber
 
 
 
-def getPaper(paper_url, filename="random_paper.pdf"):
+#optional method
+def get_paper(paper_url, filename="random_paper.pdf"):
+    """
+    - download paper according to its URL
+    :param paper_url: str
+    :param filename: str
+    :return: str
+    """
     downloadedPaper = wget.download(paper_url, filename)
     downloadedPaperFilePath = pathlib.Path(downloadedPaper)
     return downloadedPaperFilePath
 
-paperContent = pdfplumber.open("data/random_paper.pdf").pages
+paperContent = pdfplumber.open("data/random_paper.pdf").pages #already downloaded :)
 
-def displayPaperContent(paperContent, page_start=0, page_end=5):
+def display_content(paperContent, page_start=0, page_end=5):
+    """
+    -displays paper content
+
+    :param paperContent:
+    :param page_start:
+    :param page_end:
+    :return:
+    """
     for page in paperContent[page_start:page_end]:
         print(page.extract_text())
 
-
-def showPaperSummary(paperContent):
+def show_summary(paperContent):
     tldr_tag = "\n tl;dr:"
-    openai.api_key = "API-KEY"
+    openai.api_key = "YOUR API-KEY"
 
     for page in paperContent:
         text = page.extract_text() + tldr_tag
@@ -32,4 +46,4 @@ def showPaperSummary(paperContent):
                                             )
         print(response["choices"][0]["text"])
 
-showPaperSummary(paperContent)
+show_summary(paperContent)
